2d187353801ec532d1e8a977663b995241be14f8
Nick Norton <nnort9879@gmail.com>
2025-11-07 14:18:32 -0800

    Using gg for more interesting shape options. Circles rn.

--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module gotime
 go 1.25.4
 
 require github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+
+require (
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.32.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,45 +2,43 @@ package main
 
 import (
 	"image"
-	"image/color"
-	"image/draw"
 	"image/png"
 	"os"
 
+	"github.com/fogleman/gg"
 	"github.com/skip2/go-qrcode"
 )
 
 func renderQR(bitmap [][]bool, moduleSize int) *image.RGBA {
 	size := len(bitmap) * moduleSize
-	img := image.NewRGBA(image.Rect(0, 0, size, size))
 
-	// Custom colors
-	bgColor := color.RGBA{R: 248, G: 242, B: 236, A: 255} // #f8f2ec
-	fgColor := color.RGBA{R: 85, G: 32, B: 72, A: 255}    // #552048
+	dc := gg.NewContext(size, size)
 
-	// Fill background
-	draw.Draw(img, img.Bounds(), &image.Uniform{bgColor}, image.Point{}, draw.Src)
+	// Background
+	dc.SetRGB(0.973, 0.949, 0.925) // #f8f2ec
+	dc.Clear()
 
-	// Draw modules
+	// Draw modules as circles
+	dc.SetRGB(0.333, 0.125, 0.282) // #552048
 	for y := 0; y < len(bitmap); y++ {
 		for x := 0; x < len(bitmap[y]); x++ {
 			if bitmap[y][x] {
-				// Draw a square module
-				rect := image.Rect(
-					x*moduleSize, y*moduleSize,
-					(x+1)*moduleSize, (y+1)*moduleSize,
-				)
-				draw.Draw(img, rect, &image.Uniform{fgColor}, image.Point{}, draw.Src)
+				centerX := float64(x*moduleSize + moduleSize/2)
+				centerY := float64(y*moduleSize + moduleSize/2)
+				radius := float64(moduleSize) / 2.0
+
+				dc.DrawCircle(centerX, centerY, radius)
+				dc.Fill()
 			}
 		}
 	}
 
-	return img
+	return dc.Image().(*image.RGBA)
 }
 
 func main() {
 	// Generate QR code
-	q, err := qrcode.New("https://nickn.dev", qrcode.High)
+	q, err := qrcode.New("https://nickn.dev", qrcode.Highest)
 	if err != nil {
 		panic(err)
 	}
