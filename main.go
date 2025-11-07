2178432da80baf54d18496c09e5566592d8038db
Nick Norton <nnort9879@gmail.com>
2025-11-07 13:42:37 -0800

    basic circle-y qrcode leading to nickndev

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"image/color"
+
+	"github.com/yeqown/go-qrcode/v2"
+	"github.com/yeqown/go-qrcode/writer/standard"
+)
 
 func main() {
-	fmt.Println("Hello, Go!")
+	// Generate QR code
+	qrc, err := qrcode.New("https://nickn.dev")
+	if err != nil {
+		panic(err)
+	}
+
+	// Create writer with custom options
+	w, err := standard.New("output.png",
+		standard.WithBgColor(color.RGBA{R: 248, G: 242, B: 236, A: 255}), // Light beige background
+		standard.WithFgColor(color.RGBA{R: 85, G: 32, B: 72, A: 255}),    // Dark purple foreground
+		standard.WithCircleShape(),
+		standard.WithQRWidth(128), // Size of QR code
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// Save the QR code
+	if err = qrc.Save(w); err != nil {
+		panic(err)
+	}
 }
